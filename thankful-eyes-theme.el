dacd5b565e37294ec72234c7eab859f7c2ed16bf
Andros Fenollosa <andros@fenollosa.email>
2025-03-16 18:30:25 +0100

    Added tab colours

--- thankful-eyes-theme.el
+++ thankful-eyes-theme.el
@@ -54,7 +54,14 @@
  '(link-visited ((t (:foreground "#f696db" :underline t))))
  '(isearch ((t (:foreground "#122b3b" :background "#f6dd62"))))
  '(lazy-highlight ((t (:foreground "#122b3b" :background "#fff0a6"))))
- '(error ((t (:foreground "#cc0000" :weight bold)))))
+ '(error ((t (:foreground "#cc0000" :weight bold))))
+ '(tab-bar ((t (:background "#1c2f3b" :foreground "#a8e1fe" :box (:line-width 2 :color "#1c2f3b")))))
+ '(tab-bar-tab ((t (:background "#4e5d62" :foreground "#faf6e4" :box (:line-width 2 :color "#4e5d62")))))
+ '(tab-bar-tab-inactive ((t (:background "#1c2f3b" :foreground "#6c8b9f" :box (:line-width 2 :color "#1c2f3b")))))
+ '(tab-line ((t (:background "#1c2f3b" :foreground "#a8e1fe" :box (:line-width 2 :color "#1c2f3b")))))
+ '(tab-line-tab ((t (:background "#4e5d62" :foreground "#faf6e4" :box (:line-width 2 :color "#4e5d62")))))
+ '(tab-line-tab-inactive ((t (:background "#1c2f3b" :foreground "#6c8b9f" :box (:line-width 2 :color "#1c2f3b")))))
+ '(tab-line-tab-current ((t (:background "#4e5d62" :foreground "#faf6e4" :box (:line-width 2 :color "#4e5d62"))))))
 
 (provide-theme 'thankful-eyes)
 
