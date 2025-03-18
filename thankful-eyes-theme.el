66e8675ba13d7a20ce0913a2a8beee1c01649116
Andros Fenollosa <andros@fenollosa.email>
2025-03-18 08:08:20 +0100

    Fix tab line

--- thankful-eyes-theme.el
+++ thankful-eyes-theme.el
@@ -58,7 +58,7 @@
  '(tab-bar ((t (:background "#122b3b" :foreground "#a8e1fe" :box (:line-width 2 :color "#1c2f3b")))))
  '(tab-bar-tab ((t (:background "#4e5d62" :foreground "#faf6e4" :box (:line-width 2 :color "#4e5d62")))))
  '(tab-bar-tab-inactive ((t (:background "#1c2f3b" :foreground "#6c8b9f" :box (:line-width 2 :color "#1c2f3b")))))
- '(tab-line ((t (:background "#1c2f3b" :foreground "#a8e1fe" :box (:line-width 2 :color "#1c2f3b")))))
+ '(tab-line ((t (:background "#122b3b" :foreground "#a8e1fe" :box (:line-width 2 :color "#122b3b")))))
  '(tab-line-tab ((t (:background "#4e5d62" :foreground "#faf6e4" :box (:line-width 2 :color "#4e5d62")))))
  '(tab-line-tab-inactive ((t (:background "#1c2f3b" :foreground "#6c8b9f" :box (:line-width 2 :color "#1c2f3b")))))
  '(tab-line-tab-current ((t (:background "#4e5d62" :foreground "#faf6e4" :box (:line-width 2 :color "#4e5d62"))))))
